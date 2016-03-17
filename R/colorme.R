187b1cf6967e5f15845609d5d1eb7856bd96bb83
Elisabeth Bommes <elisabeth.bommes@hu-berlin.de>
2016-03-17 13:54:30 +0100

    Fix bug in colorme

--- R/colorme.R
+++ R/colorme.R
@@ -10,26 +10,26 @@
 
 colorme = function(col = NULL, numb = NULL){
 
-	scheme =  list(blue    = "#062FCD",
-	               red     = "#e31a1c", 
-	               green   = "#33a02c",
-	               orange  = "#ff7f00",
-	               pink    = "#A80BA7",
-		           lblue   = "#a6cee3",
-	               lgreen  = "#b2df8a", 
-	               lred    = "#fb9a99", 
-	               yellow  = "#DDCC77",
-	               lorange = "#fdbf6f",
-	               lgrey   = "#a8a8a8",
-	               grey    = "#696969",
-	               dgrey   = "#2a2a2a")
+    scheme =  list(blue    = "#062FCD",
+                   red     = "#e31a1c", 
+                   green   = "#33a02c",
+                   orange  = "#ff7f00",
+                   pink    = "#A80BA7",
+                   lblue   = "#a6cee3",
+                   lgreen  = "#b2df8a", 
+                   lred    = "#fb9a99", 
+                   yellow  = "#DDCC77",
+                   lorange = "#fdbf6f",
+                   lgrey   = "#a8a8a8",
+                   grey    = "#696969",
+                   dgrey   = "#2a2a2a")
 
-	if(!is.null(col)){
-		return(unlist(colsch[names(colsch) %in% col]))
-	}
+    if(!is.null(col)){
+        return(unlist(scheme[names(scheme) %in% col]))
+    }
     
     if(!is.null(numb)){
-		return(unlist(colsch)[1:numb])
-	}
+        return(unlist(scheme)[1:numb])
+    }
 
 }
