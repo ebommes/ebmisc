314213eabbd7d710162844faf1de9d9abaa94046
Elisabeth Bommes <elisabeth.bommes@hu-berlin.de>
2016-03-17 13:47:12 +0100

    Add colorme

---
+++ R/colorme.R
@@ -0,0 +1,35 @@
+#' colorme
+#'
+#' Returns colors from EB color scheme
+#' @param col Name of color.
+#' @param numb Number of colors to return
+#' @keywords file
+#' @export
+#' @examples
+#' colorme("blue")
+
+colorme = function(col = NULL, numb = NULL){
+
+	scheme =  list(blue    = "#062FCD",
+	               red     = "#e31a1c", 
+	               green   = "#33a02c",
+	               orange  = "#ff7f00",
+	               pink    = "#A80BA7",
+		           lblue   = "#a6cee3",
+	               lgreen  = "#b2df8a", 
+	               lred    = "#fb9a99", 
+	               yellow  = "#DDCC77",
+	               lorange = "#fdbf6f",
+	               lgrey   = "#a8a8a8",
+	               grey    = "#696969",
+	               dgrey   = "#2a2a2a")
+
+	if(!is.null(col)){
+		return(unlist(colsch[names(colsch) %in% col]))
+	}
+    
+    if(!is.null(numb)){
+		return(unlist(colsch)[1:numb])
+	}
+
+}
